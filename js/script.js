0cf95d7e9b532166d8e9eb4aacee695cd3873e77
Emanuele <cannone.emanuele@gmail.com>
2020-12-15 17:06:32 +0100

    btn genera + btn annulla

--- js/script.js
+++ js/script.js
@@ -5,29 +5,47 @@
 //Categoria selezionata dall'utente
 
 //dichiariamo le variabili
-var nome, km, genere, costo, numeroCarrozza, codiceCp;
+var nome, km, genere, costo, numeroCarrozza, codiceCp, message;
 
 
-//definizione ciascuna variabile
-nome = document.getElementById('nome').value;
-km = document.getElementById('km').value;
-genere = document.getElementById('genere').value;
-codiceCp = Math.floor(Math.random() * (90000 - 100000) +90000);
-document.getElementById('codiceCp').innerHTML = codiceCp;
+//definizione ciascuna variabile solamente quando l'utente fa il click su genera
+
+document.getElementById("genera").addEventListener("click", function() {
+  nome = document.getElementById('nome').value;
+  km = document.getElementById('km').value;
+  genere = document.getElementById('genere').value;
+  numeroCarrozza = Math.floor(Math.random() *9 +1);
+  document.getElementById('numeroCarrozza').innerHTML = numeroCarrozza;
+  codiceCp = Math.floor(Math.random() * (90000 - 100000) +90000);
+  document.getElementById('codiceCp').innerHTML = codiceCp;
 
 
-//calcolo del costo
-costo = km * 0.21
-if (genere == 'minorenne'){
-  costo -= costo * 0.2;
-} else if (genere == 'over65') {
-  costo -= costo * 0.4;
-}
+  //calcolo del costo
+  costo = km * 0.21;
+  message = 'maggiorenne';
+  if (genere == 'minorenne'){
+    costo -= costo * 0.2;
+    message = 'minorenne';
+  } else if (genere == 'over65') {
+    costo -= costo * 0.4;
+    message = 'premium';
+  }
 
 
-//controlla se i dati vengono presi
-//console.log(nome, km, genere, costo);
+  //visualizza negli appositi id i dati :
+  document.getElementById('nome-stampato').innerHTML = nome;
+  document.getElementById('offerta').innerHTML = message;
+  document.getElementById('costo').innerHTML = costo.toFixed(2) + '€';
 
 
+  //controlla se i dati vengono presi
+  //console.log(nome, km,  costo, message);
+});
 
-
+document.getElementById("annulla").addEventListener("click", function() {
+  document.getElementById('nome-stampato').innerHTML = ' ';
+  document.getElementById('offerta').innerHTML = ' ';
+  document.getElementById('costo').innerHTML = ' ';
+  document.getElementById('codiceCp').innerHTML = ' ';
+  document.getElementById('numeroCarrozza').innerHTML = ' ';
+});
